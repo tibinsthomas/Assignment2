8fac7d966d2de2ed20f6cd9236f83b1aa375cd36
tibinsthomas <tibinstthomas@gmail.com>
2023-02-09 08:12:43 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// function test1() {
+// 	$("h2").hide(5000);
+// }
+// $("#btn1").click(function () {
+// 	$("h2").hide(3000);
+// });
+
+// $(document).ready(() => {
+// 	$("#btn1").click(() => {
+// 		$("h2").hide(3000);
+// 	});
+// });
